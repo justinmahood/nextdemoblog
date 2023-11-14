88a7804b8b3b019f95420531e3aa0cec6a62c390
[jmahood] <[jmahood@google.com]>
2023-11-14 15:34:41 +0000

    merge

--- load.bash
+++ load.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
-workers=100
+workers=120
 qps=15
-minutes=5m
+minutes=30m
 hey -c $workers -q $qps -z $minutes https://nextdemoblog-ean27jt5ha-uc.a.run.app \
 & hey -c $workers -q $qps -z $minutes https://justinsblog.web.app
